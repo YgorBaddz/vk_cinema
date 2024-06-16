1a6279e43588f2d0ddfdc530e7d0e79c1a28d6cd
Eegor Badmayev <igorbadmaev2002@gmail.com>
2024-06-16 19:42:08 +0300

    replacing MovieDetails into pages fold

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import MovieDetails from "./components/MovieDetails.tsx";
+import MovieDetails from "./pages/MovieDetails.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
