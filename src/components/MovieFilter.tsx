5e5e30caa5a9f9ca964c915d72a0ad601619df2e
Eegor Badmayev <igorbadmaev2002@gmail.com>
2024-06-17 20:58:18 +0300

    genre options added

--- src/components/MovieFilter.tsx
+++ src/components/MovieFilter.tsx
@@ -94,11 +94,23 @@ const MovieFilter: React.FC<MovieFilterProps> = ({
           <option value="">All genres</option>
           <option value="Action">Action</option>
           <option value="Adventure">Adventure</option>
+          <option value="Animation">Animation</option>
+          <option value="Biography">Biography</option>
           <option value="Comedy">Comedy</option>
+          <option value="Crime">Crime</option>
+          <option value="Documentary">Documentary</option>
           <option value="Drama">Drama</option>
+          <option value="Family">Family</option>
+          <option value="Fantasy">Fantasy</option>
+          <option value="History">History</option>
           <option value="Horror">Horror</option>
+          <option value="Music">Music</option>
+          <option value="Mystery">Mystery</option>
           <option value="Romance">Romance</option>
+          <option value="Sci-Fi">Sci-Fi</option>
+          <option value="Sport">Sport</option>
           <option value="Thriller">Thriller</option>
+          <option value="War">War</option>
         </select>
       </div>
 
