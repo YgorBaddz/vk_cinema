c3f925204d41ac41d0b560b162827453759edf49
Eegor Badmayev <igorbadmaev2002@gmail.com>
2024-06-17 22:38:14 +0300

    refactoring filters

--- src/components/MovieFilter.tsx
+++ src/components/MovieFilter.tsx
@@ -3,6 +3,10 @@ import axios from "axios";
 import MovieList from "./MovieList";
 import { Movie, MovieFilterProps } from "../types/Types";
 import { APIKey } from "../api/APIKey";
+import SearchFilter from "./filters/SearchFilter";
+import GenreFilter from "./filters/GenreFilter";
+import YearFilter from "./filters/YearFilter";
+import RatingFilter from "./filters/RatingFilter";
 
 const MovieFilter: React.FC<MovieFilterProps> = ({
   searchString,
@@ -15,22 +19,6 @@ const MovieFilter: React.FC<MovieFilterProps> = ({
   setSelectedRating,
 }) => {
   const [filteredMovies, setFilteredMovies] = useState<Movie[]>([]);
-
-  const handleSearchInput = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setSearchString(e.target.value);
-  };
-
-  const handleGenreChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
-    setSelectedGenre(e.target.value);
-  };
-
-  const handleYearChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setSelectedYear(e.target.value);
-  };
-
-  const handleRatingChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setSelectedRating(e.target.value);
-  };
 
   useEffect(() => {
     const filterMovies = async () => {
@@ -69,65 +57,22 @@ const MovieFilter: React.FC<MovieFilterProps> = ({
 
   return (
     <div className="w-full flex flex-col items-center">
-      <div className="w-full max-w-md mb-8">
-        <input
-          type="text"
-          placeholder="Search for a movie"
-          value={searchString}
-          onChange={handleSearchInput}
-          className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-        />
-      </div>
-
-      <div className="w-full max-w-md mb-8">
-        <select
-          value={selectedGenre}
-          onChange={handleGenreChange}
-          className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-        >
-          <option value="">All genres</option>
-          <option value="Action">Action</option>
-          <option value="Adventure">Adventure</option>
-          <option value="Animation">Animation</option>
-          <option value="Biography">Biography</option>
-          <option value="Comedy">Comedy</option>
-          <option value="Crime">Crime</option>
-          <option value="Documentary">Documentary</option>
-          <option value="Drama">Drama</option>
-          <option value="Family">Family</option>
-          <option value="Fantasy">Fantasy</option>
-          <option value="History">History</option>
-          <option value="Horror">Horror</option>
-          <option value="Music">Music</option>
-          <option value="Mystery">Mystery</option>
-          <option value="Romance">Romance</option>
-          <option value="Sci-Fi">Sci-Fi</option>
-          <option value="Sport">Sport</option>
-          <option value="Thriller">Thriller</option>
-          <option value="War">War</option>
-        </select>
-      </div>
-
-      <div className="w-full max-w-md mb-8">
-        <input
-          type="text"
-          placeholder="Filter by year"
-          value={selectedYear}
-          onChange={handleYearChange}
-          className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-        />
-      </div>
-
-      <div className="w-full max-w-md mb-8">
-        <input
-          type="text"
-          placeholder="Filter by rating"
-          value={selectedRating}
-          onChange={handleRatingChange}
-          className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-        />
-      </div>
-
+      <SearchFilter
+        searchString={searchString}
+        setSearchString={setSearchString}
+      />
+      <GenreFilter
+        selectedGenre={selectedGenre}
+        setSelectedGenre={setSelectedGenre}
+      />
+      <YearFilter
+        selectedYear={selectedYear}
+        setSelectedYear={setSelectedYear}
+      />
+      <RatingFilter
+        selectedRating={selectedRating}
+        setSelectedRating={setSelectedRating}
+      />
       {filteredMovies && filteredMovies.length > 0 ? (
         <MovieList movies={filteredMovies} />
       ) : (
